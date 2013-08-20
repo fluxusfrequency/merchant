425d4a7ef0cf8dc45a9da45bbf581aa1827311fc
Fluxusfrequency <bennlewis@gmail.com>
2013-08-20 07:02:53 -0600

    Generated rails scaffold for products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merchant::Application.config.secret_token = '9e8623e413a3dfea54cc2941e0205a147b4169b0d75e1867989e85de202c6eea47e3d2bb044f34107a29ebfc2eab631a39666c4484ed433f949d988843dea015'
