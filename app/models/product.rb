cd744f138f4c990510defa5b3cf86e4e42467d59
Fluxusfrequency <bennlewis@gmail.com>
2013-08-20 07:36:56 -0600

    Added table formatting, price formatting

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates_numericality_of :price
+
+    def price=(input)
+    input.delete!("$")
+    super
+  end
+
 end
